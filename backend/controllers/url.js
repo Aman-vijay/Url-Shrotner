0a4726206be5f3aad998201dd4049648bb13f3fb
Aman-vijay <shivamvj04@gmail.com>
2025-03-20 10:17:54 +0530

    implement URL redirection and analytics features in the backend

--- backend/controllers/url.js
+++ backend/controllers/url.js
@@ -2,19 +2,63 @@ const {nanoid} = require("nanoid");
 const URL = require("../models/url")
 
 async function GenerateNewUrl(req,res) {
-    const body = req.body;
-    if(!body.url) return res.status(400).json({error:"url is required"})
+    try{
+    const {body} = req;
+    const redirectUrI = body.url
+    if(!redirectUrI) return res.status(400).json({error:"url is required"})
+    const existingURL = await URL.findOne({ redirectUrl: redirectUrI });
+    if (existingURL) {
+        return res.json({ id: existingURL.shortUrl });
+    }
     const shortId = nanoid(8);
     await URL.create({
         shortUrl:shortId,
-        redirectUrl:body.url,
+        redirectUrl:redirectUrI,
         visitHistory:[],
 
     });
 
     return res.json({id:shortId});
+}
+catch(err){
+    return res.status(404).json({err:"Something went wrong"})
+}
 
 
 }
 
-module.exports = {GenerateNewUrl};
+async function redirectIdtoUrl(req,res){
+    try{
+    const shortId = req.params.shortId;
+const entry = await URL.findOneAndUpdate(
+    { shortUrl: shortId },
+    { $push: { visitHistory: { timestamp: Date.now() } } },
+    { new: true }
+);
+
+if (!entry) {
+    return res.status(404).json({ error: "URL not found" });
+}
+   res.redirect(entry.redirectUrl);
+}
+catch(err){
+    res.status(404).json({err:"Something went wrong"})
+}
+}
+
+async function showAnalytics(req,res){
+    try{
+         const shortId = req.params.shortId;
+         const count = await URL.findOne({
+            shortUrl:shortId,
+         })
+
+         return res.json({totalClicks:count.visitHistory.length});
+
+    }
+    catch(err){
+        res.status(404).json({err:"Something went wrong"})
+    }
+}
+
+module.exports = {GenerateNewUrl,redirectIdtoUrl,showAnalytics};
--- backend/routes/url.js
+++ backend/routes/url.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router()
-const {GenerateNewUrl} = require("../controllers/url")
+const {GenerateNewUrl,redirectIdtoUrl,showAnalytics} = require("../controllers/url")
 
 router.post("/",GenerateNewUrl);
+
+router.get("/:shortId", redirectIdtoUrl);
+
+router.get("/analytics/:shortId", showAnalytics);
 
 module.exports = router;
