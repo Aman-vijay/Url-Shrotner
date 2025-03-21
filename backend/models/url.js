102c41aeff194dd7289301b8b16c59e182e816f2
Aman-vijay <shivamvj04@gmail.com>
2025-03-21 12:08:26 +0530

    Add analytics schema to track URL click data

--- backend/models/url.js
+++ backend/models/url.js
@@ -1,5 +1,43 @@
 const mongoose = require("mongoose");
 
+const AnalyticsSchema = new mongoose.Schema({
+    urlId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"url",
+        required:true
+    },
+    clicks: [
+        {
+            Timestamp:{
+                type:Number,
+                default:Date.now(),
+
+            },
+            ip:{
+                type:String,
+                default: null
+            },
+            userAgent:{
+                type:String,
+                default:null
+            },
+            city:{
+                type:String,
+                default:null
+            },
+            country:{
+                type:String,
+                default:null
+            },
+            deviceType: { type: String, default: null },
+            latitude: { type: Number, default: null },
+            longitude: { type: Number, default: null }
+
+        }
+    ]
+
+},{timestamps:true})
+
 const UrlSchema = new mongoose.Schema({
     shortUrl : {
         type:String,
@@ -22,14 +60,10 @@ const UrlSchema = new mongoose.Schema({
     qr:{
         type:String,
         unique:true
-    },
-    visitHistory:[
-        {
-            Timestamp:{type:Number}
-        }
-    ]
+    }
 },{timestamps:true})
 
 const URL = mongoose.model("url",UrlSchema)
+const Analytics = mongoose.model("analytics",AnalyticsSchema)
 
-module.exports = URL
+module.exports = {URL, Analytics}
