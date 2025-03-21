c93d09cc3e8cd6ac8b06788a56a790786153a7e4
Aman-vijay <shivamvj04@gmail.com>
2025-03-21 12:09:25 +0530

    Add token verification middleware to URL routes

--- backend/routes/url.js
+++ backend/routes/url.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router()
 const {GenerateNewUrl,redirectIdtoUrl,showAnalytics} = require("../controllers/url")
+const {verifyTokenMiddleware} = require("../utils/jwt")
 
-router.post("/",GenerateNewUrl);
+router.post("/",verifyTokenMiddleware, GenerateNewUrl);
 
 router.get("/:shortId", redirectIdtoUrl);
 
-router.get("/analytics/:shortId", showAnalytics);
+router.get("/analytics/:shortId", verifyTokenMiddleware, showAnalytics);
 
 module.exports = router;
