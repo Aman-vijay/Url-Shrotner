db9dee63c533e4162d3658cb255ca4dc8f29f460
Aman-vijay <shivamvj04@gmail.com>
2025-03-22 22:07:47 +0530

    Add routing and layout components for link shortener application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,29 @@
+import {createBrowserRouter,createRoutesFromElements,Route, RouterProvider} from "react-router-dom"
+import AppLayout from "./layouts/AppLayout"
+import Auth from "./pages/Auth"
+import Dashboard from "./pages/Dashboard"
+import Landing from "./pages/Landing"
+import Link from "./pages/Link"
+import Redirect from "./pages/Redirect"
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<AppLayout/>}>
+      <Route path="" element={<Landing/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
+      <Route path="/auth" element={<Auth/>}/>
+      <Route path="/link/:id" element={<Link/>}/>
+      <Route path="/:id" element={<Redirect/>}/>
 
 
-function App() {
-  // const [count, setCount] = useState(0)
+    </Route>
 
-  return (
-    <>
-    <div>
-      <h1 className=" text-4xl underline m-6">Hello</h1>
-    </div>
-    
-    </>
   )
+)
+function App() {
+
+
+
+  return <RouterProvider router={router}></RouterProvider>
 }
 
 export default App
---
+++ frontend/src/layouts/AppLayout.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import {Outlet} from "react-router-dom"
+
+const AppLayout = () => {
+  return (
+    <div>
+        <main>
+            <Header/>
+            <Outlet/>
+
+        </main>
+        <Footer/>
+
+
+    </div>
+  )
+}
+
+export default AppLayout
