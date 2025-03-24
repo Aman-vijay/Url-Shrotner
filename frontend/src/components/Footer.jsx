b98c5ac5d8934453ac2ec1a78dc8605181eb69e2
Aman-vijay <shivamvj04@gmail.com>
2025-03-24 23:53:19 +0530

    Refactor Footer component for improved styling and consistency

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className=" bottom-0 w-full bg-gradient-to-r from-blue-600 to-purple-600 py-4 px-6 shadow-lg">
-            <div className="container mx-auto flex items-center  justify-between">
+        <footer className="w-full bg-gradient-to-r from-blue-600 to-purple-600 py-4 px-6 shadow-lg">
+            <div className="container mx-auto flex items-center justify-between">
                 <p className="text-white text-center font-medium">
                     Made with <span className="animate-pulse inline-block">💖</span> by Aman Vijay
                 </p>
-                <p>&copy; {new Date().getFullYear()} URL Shortener. All rights reserved.</p>
+                <p className="text-white">&copy; {new Date().getFullYear()} URL Shortener. All rights reserved.</p>
             </div>
-            
         </footer>
     );
 };
