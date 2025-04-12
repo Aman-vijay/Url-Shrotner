dc2069ccb7a6f71eaf868cc6740b3d5abe70b99c
Aman-vijay <shivamvj04@gmail.com>
2025-04-12 11:47:40 +0530

    Add vercel analytics

--- frontend/src/layouts/AppLayout.jsx
+++ frontend/src/layouts/AppLayout.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import { Outlet } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 
 const AppLayout = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-grow">
+
         <Outlet />
+      <Analytics />
       </main>
       <Footer />
     </div>
